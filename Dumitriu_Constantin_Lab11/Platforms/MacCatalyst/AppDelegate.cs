92080a297091e5b61470536eedd7dff0fda9ac0f
DumitriuConstantin11 <titirex22@gmail.com>
2024-12-22 12:39:09 +0200

    Add project files.

---
+++ Dumitriu_Constantin_Lab11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Dumitriu_Constantin_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
